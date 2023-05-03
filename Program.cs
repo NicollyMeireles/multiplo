d1b6410c76e5cfcecf64c55bdc14b5c016a1724c
NicollyMeireles <Nicollymeireles32@gmail.com>
2023-05-02 21:32:55 -0300

    multiplo

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿﻿Console.WriteLine("-- Números Múltiplos --");
+Console.WriteLine("");
+Console.WriteLine("Digite um número...");
+
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite um número para comparar se ele é múltiplo ou não...");
+
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+if (numero1 % numero2 == 0)
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"O número {numero1} é múltiplo de {numero2}!");
+    Console.WriteLine("");
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"O número escrito ({numero1}) não é múltiplo do outro número escrito ({numero2})!");
+    Console.WriteLine("");
+}
+Console.ResetColor();
